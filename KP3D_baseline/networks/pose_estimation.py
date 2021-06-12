87fa1dbc0f14fb24c1ea88b11cf20e0446b2a4f1
eralpkocas <heralpkocas@gmail.com>
2021-06-12 13:56:53 +0200

    kp3d pose estimation is fixed to work with multiple batches and reshapes are also fixed

--- KP3D_baseline/networks/pose_estimation.py
+++ KP3D_baseline/networks/pose_estimation.py
@@ -3,24 +3,25 @@ import kornia
 
 
 class PoseEstimation:
-    def __init__(self, K1, K2):
+    def __init__(self, K1, K2, cuda):
         # TODO: check this K1&2 is correct or not!
         self.K1 = K1[:, :3, :3]
         self.K2 = K2[:, :3, :3]
+        self.device = torch.device("cpu" if cuda else "cuda")
 
     def match_keypoints(self, kp1, kp2, des1, des2):
         match_dist, match_idx = kornia.feature.match_mnn(des1, des2)
         match_kp1 = kp1[match_idx[:, 0]]
         match_kp2 = kp2[match_idx[:, 1]]
-        # TODO: make this reshape more beautiful!
-        match_kp1 = match_kp1.reshape((match_idx.shape[0], match_kp1.shape[0], match_kp1.shape[1]))
-        match_kp2 = match_kp2.reshape((match_idx.shape[0], match_kp2.shape[0], match_kp2.shape[1]))
+
+        match_kp1 = torch.unsqueeze(match_kp1, dim=0)
+        match_kp2 = torch.unsqueeze(match_kp2, dim=0)
+
         return match_kp1, match_kp2
 
     def find_essential_matrix(self, match_kp1, match_kp2):
-        # TODO: check torch.ones() part: how do we decide on matched keypoints' weights??
         fun_mat = kornia.geometry.find_fundamental(match_kp1, match_kp2,
-                                                   torch.ones((match_kp1.shape[0], match_kp1.shape[1])).to("cuda"))
+                                                   torch.ones((match_kp1.shape[0], match_kp1.shape[1])).to(self.device))
 
         ess_mat = kornia.geometry.essential_from_fundamental(fun_mat, self.K1, self.K2)
         return ess_mat
@@ -29,11 +30,23 @@ class PoseEstimation:
         return kornia.geometry.motion_from_essential_choose_solution(ess_mat, self.K1, self.K2, kp1, kp2)
 
     def get_pose(self, kp1, kp2, des1, des2):
-        match_kp1, match_kp2 = self.match_keypoints(kp1, kp2, des1, des2)
-        ess_mat = self.find_essential_matrix(match_kp1, match_kp2)
+        kp1 = torch.unsqueeze(kp1, dim=0)
+        kp2 = torch.unsqueeze(kp2, dim=0)
+        des1 = torch.unsqueeze(des1, dim=0)
+        des2 = torch.unsqueeze(des2, dim=0)
 
-        kp1 = kp1.reshape((1, kp1.shape[0], kp1.shape[1])) # TODO: make these lines more beautiful :D
-        kp2 = kp2.reshape((1, kp2.shape[0], kp2.shape[1]))
+        outputs_R = torch.tensor([]).to(self.device)
+        outputs_t = torch.tensor([]).to(self.device)
+        for i in range(kp1.shape[0]):
+            kp1 = kp1[i, :, :]
+            kp2 = kp2[i, :, :]
+            des1 = des1[i, :, :]
+            des2 = des2[i, :, :]
 
-        R, t, tri_points = self.get_six_dof(ess_mat, kp1, kp2)
-        return R, t
+            match_kp1, match_kp2 = self.match_keypoints(kp1, kp2, des1, des2)
+            ess_mat = self.find_essential_matrix(match_kp1, match_kp2)
+
+            R, t, tri_points = self.get_six_dof(ess_mat, match_kp1, match_kp2)
+            outputs_R = torch.cat((outputs_R, R), dim=0)
+            outputs_t = torch.cat((outputs_t, t), dim=0)
+        return outputs_R, outputs_t
