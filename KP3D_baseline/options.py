6362350e5923e64a78c66f48fdcaef6ad2d1e81a
PSRahul <ge73sed@mytum.de>
2021-06-14 00:53:36 +0200

    cuda change

--- KP3D_baseline/options.py
+++ KP3D_baseline/options.py
@@ -115,7 +115,7 @@ class KP3DOptions:
         self.parser.add_argument("--batch_size",
                                  type=int,
                                  help="batch size",
-                                 default=14) # 12
+                                 default=12) # 12
         self.parser.add_argument("--learning_rate",
                                  type=float,
                                  help="learning rate",
