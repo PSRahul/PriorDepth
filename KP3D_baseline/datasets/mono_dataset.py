d7ece47cc127bb66d44facf240d1017f8fe25648
Konstantin Blaschke <konstantin.blaschke@tum.de>
2021-07-01 13:00:05 +0200

    incorporating md2 change in branch

--- KP3D_baseline/datasets/mono_dataset.py
+++ KP3D_baseline/datasets/mono_dataset.py
@@ -106,7 +106,12 @@ class MonoDataset(data.Dataset):
             if "color" in k:
                 n, im, i = k
                 inputs[(n, im, i)] = self.to_tensor(f)
-                inputs[(n + "_aug", im, i)] = self.to_tensor(color_aug(f))
+                if(callable(color_aug)):
+                    inputs[(n + "_aug", im, i)] = self.to_tensor(color_aug(f))
+                else:
+                    values = color_aug[1:]
+                    brightness, saturation, contrast, hue = values
+                    inputs[(n + "_aug", im, i)] = self.to_tensor(transforms.ColorJitter(brightness, saturation, contrast, abs(hue)).forward(f))
 
     def __len__(self):
         return len(self.filenames)
@@ -171,9 +176,7 @@ class MonoDataset(data.Dataset):
             inputs[("inv_K", scale)] = torch.from_numpy(inv_K)
 
         if do_color_aug:
-            #color_aug = transforms.ColorJitter.get_params(
-            #    self.brightness, self.contrast, self.saturation, self.hue)
-            color_aug = transforms.ColorJitter(self.brightness, self.contrast, self.saturation, self.hue)
+            color_aug = transforms.ColorJitter.get_params(self.brightness, self.contrast, self.saturation, self.hue)
         else:
             color_aug = (lambda x: x)
 
